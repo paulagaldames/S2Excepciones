6175694df22d9e03e9a2ca9025db232a8550faa7
Paula Galdames <sitio.galdames@gmail.com>
2025-06-14 21:56:25 -0400

    Armado del frame con sus elementos.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //armando el marco
+        JFrame frame = new JFrame("Calculadora de área del tringulo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,200);
+        JPanel mainPanel = new JPanel(new GridLayout(3,2,15,15));
+        //mainPanel.setBackground(Color.GRAY); //esto cambia el fondo
+        mainPanel.setBorder(new EmptyBorder(20,20,20,20));
+
+        //etiquetas label
+        JLabel baseLabel = new JLabel("Base: ");
+        JLabel areaLabel = new JLabel("Area: ");
+
+        //entrada de los textos
+        JTextField baseText = new JTextField();
+        JTextField areaText = new JTextField();
+
+        //botón
+        JButton submitButton = new JButton("Calcular");
+
+        //agregamos los componentes al formulario
+        mainPanel.add(baseLabel);
+        mainPanel.add(baseText);
+        mainPanel.add(areaLabel);
+        mainPanel.add(areaText);
+        mainPanel.add(submitButton);
+
+
+        frame.add(mainPanel);
+        frame.setVisible(true);
     }
 }
